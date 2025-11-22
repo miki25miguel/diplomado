d2a29a2a5f79e62384d810bf290d2137be178a3f
miki25miguel <miki25.miguel@gmail.com>
2025-11-22 11:45:45 -0700

    Update 03 Dashboard con Mapas.py

--- 03 Dashboard con Mapas.py
+++ 03 Dashboard con Mapas.py
@@ -1,11 +1,9 @@
-#!pip install streamlit pyngrok
-#pip install streamlit pyngrok
+!pip install streamlit pyngrok
 
 from pyngrok import ngrok
 ngrok.set_auth_token("35qJAGN9pDjSLGWbj60RqG13IlY_6mpxt5Zix41yDvGyqJ5bX")
 
-#%%writefile app.py
-
+%%writefile app.py
 import streamlit as st
 import pandas as pd
 import plotly.express as px
@@ -16,21 +14,6 @@ import plotly.express as px
 import numpy as np
 
 st.title("🗺️ Dashboard con Mapas")
-
-# Datos
-df = px.data.gapminder().query("year == 2007")[["country", "iso_alpha", "continent"]]
-df["valor"] = np.random.randint(10, 100, size=len(df))
-
-fig = px.choropleth(
-    df,
-    locations="iso_alpha",
-    color="valor",
-    hover_name="country",
-    projection="natural earth",
-    title="Mapa Mundial con Valores Aleatorios"
-)
-
-st.plotly_chart(fig)
 
 from pyngrok import ngrok
 import time
